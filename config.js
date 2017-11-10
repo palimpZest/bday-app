6b98e9dc1196c5de3973b9c56b7ebf5de16e15bd
Emmanuel Plougoulm <contact@emmanuelplougoulm.fr>
2017-11-10 13:57:01 +0100

    add google maps

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     host     : 'localhost',
     user     : 'root',
-    password : 'yesmysqlyes',
+    password : 'root',
     database : 'bday'
 };
---
+++ public/javascript/script.js
@@ -0,0 +1,15 @@
+
+
+     
+
+    var map;
+    
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {
+                lat: 48.856,
+                lng: 2.352
+            },
+            zoom: 8
+        });
+    }
